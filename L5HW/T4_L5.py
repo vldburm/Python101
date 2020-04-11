595c70bd97f666e16094f1e30346b812632011ab
Vladislav Burmistrov <bvv93@icloud.com>
2020-04-11 15:18:52 +0300

    Lesson05

---
+++ L5HW/T4_L5.py
@@ -0,0 +1,22 @@
+"""
+Task 4
+Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные. При этом английские
+числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
+"""
+
+trans = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
+new_file = []
+with open('T4_L5.txt', 'r') as file_obj:
+    #content = file_obj.read().split('\n')
+    for i in file_obj:
+        i = i.split(' ', 1)
+        new_file.append(trans[i[0]] + '  ' + i[1])
+    print(new_file)
+
+with open('T4_L5_new.txt', 'w') as file_obj_2:
+    file_obj_2.writelines(new_file)
